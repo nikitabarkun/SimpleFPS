ac6d610dbab4c0c16b4b60cb28c2d30d5dcb2a0e
nbarkun <nikitaba@playtika.com>
2020-10-14 16:01:30 +0300

    Implemented player controllers

---
+++ SimpleFPS/Assets/Scripts/BaseCharacterController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class BaseCharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private KeyCode forwardKey = KeyCode.W;
+    [SerializeField]
+    private KeyCode backKey = KeyCode.S;
+    [SerializeField]
+    private KeyCode leftKey = KeyCode.A;
+    [SerializeField]
+    private KeyCode rightKey = KeyCode.D;
+
+    private Transform _transform;
+    private Transform _cameraTransform;
+
+    private float _previousY;
+
+    protected void Awake()
+    {
+        _transform = transform;
+        _cameraTransform = GetComponentInChildren<Camera>().transform;
+
+        _previousY = _transform.position.y;
+    }
+
+    protected void Update()
+    {
+        Move();
+    }
+
+    protected void Move()
+    {
+        if (Input.GetKey(forwardKey))
+        {
+            _transform.position += _cameraTransform.forward * Time.deltaTime;
+        }
+
+        if (Input.GetKey(backKey))
+        {
+            _transform.position -= _cameraTransform.forward * Time.deltaTime;
+        }
+
+        if (Input.GetKey(leftKey))
+        {
+            _transform.position -= _cameraTransform.right * Time.deltaTime;
+        }
+
+        if (Input.GetKey(rightKey))
+        {
+            _transform.position += _cameraTransform.right * Time.deltaTime;
+        }
+
+        _transform.position = new Vector3(_transform.position.x, _previousY, _transform.position.z);
+    }
+}
---
+++ SimpleFPS/Assets/Scripts/MouseController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    private const float Sensitivity = 3F;
+    private const float MaxAngleDeviation = 30F;
+
+    private Vector2 _relativeRotation = Vector2.zero;
+
+    private Transform _transform;
+    private Transform _cameraTransform;
+
+    protected void Awake()
+    {
+        _transform = transform;
+        _cameraTransform = GetComponentInChildren<Camera>().transform;
+    }
+
+    private void Update()
+    {
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        _relativeRotation.x += -Input.GetAxis("Mouse Y");
+        _relativeRotation.x = Mathf.Clamp(_relativeRotation.x, -MaxAngleDeviation, MaxAngleDeviation);
+
+        _relativeRotation.y += Input.GetAxis("Mouse X");
+
+        _transform.eulerAngles = new Vector2(0, _relativeRotation.y) * Sensitivity;
+        _cameraTransform.localRotation = Quaternion.Euler(_relativeRotation.x * Sensitivity, 0, 0);
+    }
+}
