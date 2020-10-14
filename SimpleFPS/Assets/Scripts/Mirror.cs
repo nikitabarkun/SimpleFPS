e20a49976b8148e34a73e1cb520729b1d8389bac
nbarkun <nikitaba@playtika.com>
2020-10-14 18:03:01 +0300

    Added mirror

---
+++ SimpleFPS/Assets/Scripts/Mirror.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirror : MonoBehaviour
+{
+    private Renderer _renderer;
+    private Camera _camera;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+        _camera = GetComponentInChildren<Camera>();
+
+        var texture = new RenderTexture(256, 256, 16, RenderTextureFormat.ARGB32);
+        texture.Create();
+
+        _renderer.material.SetTexture("_MainTex", texture);
+
+        _camera.targetTexture = texture;
+    }
+}
